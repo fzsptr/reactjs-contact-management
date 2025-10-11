b1a16ad94da3fbafed8525684995d706a647b014
fzsptr <fzsptr@localhost.localdomain>
2025-10-12 02:33:59 +0700

    add register, login, dashboard page

---
+++ frontend/src/lib/api/detailApi.js
@@ -0,0 +1,11 @@
+const userDetail = async(token) => {
+    return await fetch(`${import.meta.env.VITE_API_PATH}/users/current`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': token
+        }
+    })
+}
+
+export default userDetail;
