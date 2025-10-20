75887b6b8f6550e232c2b71c7e84f3a0eae3e5b7
fzsptr <fzsptr@localhost.localdomain>
2025-10-21 01:44:17 +0700

    add feature edit contacts on contacts list page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,8 @@ import UserProfile from './components/User/UserProfile.jsx'
 import UserLogout from './components/User/UserLogout.jsx'
 import ContactCreate from './components/Contact/ContactCreate.jsx'
 import ContactList from './components/Contact/ContactList.jsx'
+import ContactEdit from './components/Contact/ContactEdit.jsx'
+import ContactDetail from './components/Contact/ContactDetail.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -30,6 +32,8 @@ createRoot(document.getElementById('root')).render(
           <Route path='contacts'>
             <Route index element={<ContactList/>}/>
             <Route path='create' element={<ContactCreate/>}/>
+            <Route path=':id/edit' element={<ContactEdit/>}/>
+            <Route path=':id/detail' element={<ContactDetail/>}/>
           </Route>
 
         </Route>
